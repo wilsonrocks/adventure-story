b8a0702fccbe167d584d7d0bc718ddfcbc7a0c97
James Wilson <jwilsoncalderdale@gmail.com>
2019-01-31 23:31:07 +0000

    make own app component

---
+++ src/components/app/app.container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppComponent from './app.component';
+
+class App extends React.Component {
+  render() {
+    return <AppComponent />;
+  }
+}
+
+export default App;
