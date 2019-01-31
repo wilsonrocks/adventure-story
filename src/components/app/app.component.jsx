6be60154bfe19544acd637f5bfa9a677931e7f4d
James Wilson <jwilsoncalderdale@gmail.com>
2019-01-31 23:57:51 +0000

    add node-sass and setup grid layout

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import './app.scss';
 
-const App = () => <div>BLAHBLAH</div>;
+const App = () => (
+  <div className="app-grid">
+    <div className="logo">LOGO</div>
+    <div className="character">CHARACTER</div>
+    <div className="story">STORY</div>
+    <div className="choices">CHOICES</div>
+    <div className="stats">STATS</div>
+    <div className="unknown1">UNKNOWN1</div>
+    <div className="unknown2">UNKNOWN2</div>
+  </div>
+);
 
 export default App;
