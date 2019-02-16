be5009398147b5047fa5b0b7f3fdb813fb9eb1c3
James Wilson <jwilsoncalderdale@gmail.com>
2019-02-16 07:38:18 +0000

    add isRequired property to schema checker factory.

--- src/components/character/character.component.jsx
+++ src/components/character/character.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import schemaPropType from '../../schema/schemaPropType';
+import matchSchema from '../../schema/schemaPropType';
+import { characterProperties } from '../../schema/schema';
 
-const Character = () => (
-  <div className="character">CHARACTER</div>
+const Character = ({ characterState }) => (
+  <div className="character">
+    {JSON.stringify(characterState)}
+  </div>
 );
 
 Character.propTypes = {
+  characterState: matchSchema(characterProperties).isRequired,
 };
 
 export default Character;
--- src/schema/schemaPropType.js
+++ src/schema/schemaPropType.js
@@ -3,17 +3,23 @@ import Ajv from 'ajv';
 function matchSchema(schema) {
   const ajv = new Ajv({ allErrors: true });
   const validate = ajv.compile(schema);
-  const errorString = validate.errors
-    .map(error => error.message)
-    .join('\n');
-  return (props, propName, componentName) => {
+  const schemaChecker = (props, propName, componentName) => {
     if (!validate(props[propName])) {
-      return null;
+      const errorString = validate.errors
+        .map(error => error.message)
+        .join('\n');
+      return new Error(
+        `prop ${propName} of component ${componentName} has the following problems:\n${errorString}`,
+      );
     }
-    return new Error(
-      `prop ${propName} of component ${componentName} has the following problems:\n${errorString}`,
-    );
+    return undefined;
   };
+  schemaChecker.isRequired = (props, propName, componentName) => {
+    const prop = props[propName];
+    if (prop === undefined) return new Error(`Prop ${propName} was missing on component ${componentName} when it was marked as required.`);
+    return schemaChecker(props, propName, componentName);
+  };
+  return schemaChecker;
 }
 
 export default matchSchema;
