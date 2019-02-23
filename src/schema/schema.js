5630214ade6b36e03962fe0156c64e5d16b2c121
James Wilson <jwilsoncalderdale@gmail.com>
2019-02-23 08:44:00 +0000

    use JSON schema  and allOf to split off character properties so they can be PropTyped separately

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,57 +1,77 @@
 const fullSchema = {
-  type: 'object',
-  properties: {
-    characterName: {
-      type: 'string',
-      minLength: 1,
-    },
-    characterInventory: {
-      type: 'array',
-      items: {
-        type: 'string',
+  definitions: {
+    character: {
+      type: 'object',
+      properties: {
+        characterName: {
+          type: 'string',
+          minLength: 1,
+        },
+        characterInventory: {
+          type: 'array',
+          items: {
+          },
+        },
+        characterRace: {
+          type: 'string',
+          enum: ['human', 'dwarf', 'elf'],
+        },
       },
+      required: [
+        'characterName',
+        'characterInventory',
+        'characterRace',
+      ],
     },
-    characterRace: {
-      type: 'string',
-      enum: ['human', 'dwarf', 'elf'],
-    },
-    foeKind: {
-      type: 'string',
-      enum: ['orc', 'giraffe'],
-    },
-    exits: {
-      type: 'array',
-      items: {
-        type: 'string',
+    nonCharacter: {
+      type: 'object',
+      properties: {
+        foeKind: {
+          type: 'string',
+          enum: ['orc', 'giraffe'],
+        },
+        exits: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+        loot: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+        gameStatus: {
+          type: 'string',
+          enum: ['inProgress', 'won', 'lost', 'wantsToQuit', 'quit'],
+        },
+        story: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
       },
-    },
-    loot: {
-      type: 'array',
-      items: {
-        type: 'string',
-      },
-    },
-    gameStatus: {
-      type: 'string',
-      enum: ['inProgress', 'won', 'lost', 'wantsToQuit', 'quit'],
-    },
-    story: {
-      type: 'array',
-      items: {
-        type: 'string',
-      },
+      required: [
+        'foeKind',
+        'exits',
+        'loot',
+        'gameStatus',
+        'story',
+      ],
     },
   },
-  required: [
-    'characterName',
-    'characterInventory',
-    'characterRace',
-    'foeKind',
-    'exits',
-    'loot',
-    'gameStatus',
-    'story',
+
+  allOf: [
+    {
+      $ref: '#/definitions/character',
+    },
+    {
+      $ref: '#/definitions/nonCharacter',
+    },
   ],
+
 };
 
 export default fullSchema;
